976368e01c919d3e1a48673baf0c5aaf7e0937cc
Yi Wang <logins.yiwang@gmail.com>
2020-04-19 00:55:01 -0400

    finish BST

--- BST.playground/Contents.swift
+++ BST.playground/Contents.swift
@@ -98,7 +98,126 @@ public extension BinarySearchTree {
             return self
         }
     }
+    
+    func traverseInOrder(process: (T) -> Void) {
+        left?.traverseInOrder(process: process)
+        process(value)
+        right?.traverseInOrder(process: process)
+    }
+    
+    func traversePreOrder(process: (T) -> Void) {
+        process(value)
+        left?.traversePreOrder(process: process)
+        right?.traversePreOrder(process: process)
+    }
+    
+    func traversePostOrder(process: (T) -> Void) {
+        left?.traversePostOrder(process: process)
+        right?.traversePostOrder(process: process)
+        process(value)
+    }
+    
+    func map(formula:(T) -> T) -> [T] {
+        var a = [T]()
+        if let left = left {
+            a += left.map(formula: formula)
+        }
+        a.append(formula(value))
+        if let right = right {
+            a += right.map(formula: formula)
+        }
+        return a
+    }
+    
+    func toArray() -> [T] {
+        map { $0 }
+    }
+    
+    func minimum() -> BinarySearchTree? {
+        var node = self
+        while let next = node.left {
+            node = next
+        }
+        return node
+    }
+    
+    func maximum() -> BinarySearchTree? {
+        var node = self
+        while let next = node.right {
+            node = next
+        }
+        return node
+    }
+    
+    @discardableResult
+    func remove() -> BinarySearchTree? {
+        let replacement: BinarySearchTree?
+        // Replacement for current node can be either biggest one on the left or
+        // smallest one on the right, whichever is not nil
+        if let left = left {
+            replacement = left.maximum()
+        } else if let right = right {
+            replacement = right.minimum()
+        } else {
+            replacement = nil
+        }
+        replacement?.remove()
+        
+        // Place the replacement on current node's position
+        replacement?.right = right
+        replacement?.left = left
+        right?.parent = replacement
+        left?.parent = replacement
+        reconnectParentTo(node:replacement)
+        // The current node is no longer part of the tree, so clean it up.
+        parent = nil
+        left = nil
+        right = nil
+
+        return replacement
+    }
+    
+    func height() -> Int {
+      if isLeaf {
+        return 0
+      } else {
+        return 1 + max(left?.height() ?? 0, right?.height() ?? 0)
+      }
+    }
+    
+    /// Distance to the root
+    func depth() -> Int {
+      var node = self
+      var edges = 0
+      while let parent = node.parent {
+        node = parent
+        edges += 1
+      }
+      return edges
+    }
+    
+    func isBST(minValue: T, maxValue: T) -> Bool {
+      if value < minValue || value > maxValue { return false }
+      let leftBST = left?.isBST(minValue: minValue, maxValue: value) ?? true
+      let rightBST = right?.isBST(minValue: value, maxValue: maxValue) ?? true
+      return leftBST && rightBST
+    }
 }
+
+extension BinarySearchTree {
+    /// Takes the parent of the current node -- that is self -- and connects it to another node which will be one of the children of self.
+    private func reconnectParentTo(node: BinarySearchTree?) {
+        guard let parent = parent else { return }
+        if isLeftChild {
+            parent.left = node
+        } else {
+            parent.right = node
+        }
+        node?.parent = parent
+    }
+}
+
+
 
 extension BinarySearchTree: CustomStringConvertible {
     public var description: String {
@@ -119,4 +238,11 @@ let tree = BinarySearchTree<Int>(array: [7, 2, 5, 10, 9, 1])
 tree.search(6)
 tree.search(10)
 
+print("pre order")
+tree.traversePreOrder { (value) in print(value) }
+print("in order")
+tree.traverseInOrder { (value) in print(value) }
+print("post order")
+tree.traversePostOrder { (value) in print(value) }
+tree.toArray()
 
