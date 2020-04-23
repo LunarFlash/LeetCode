2a955f8f06a418f20b99819b14715ff22767bab6
Terry Wang <logins.yiwang@gmail.com>
2020-04-23 16:07:48 -0400

    shortened

--- LongestPalindromeSubstring.playground/Contents.swift
+++ LongestPalindromeSubstring.playground/Contents.swift
@@ -18,49 +18,32 @@ class Solution {
     func longestPalindrome(_ s: String) -> String {
         if s.count <= 1 { return s }
         var str = Array(s)
-        var longest = String()
+        var longest = String(str[0])
         var curr = 0
         while curr < str.count {
-            // iterate on i until we reach a valid palindrome (aa or aba), then expand and store.
-            let palindromeFound = expandAround(curr: &curr, str: &str) // at min this will be a pathetic 1 char palindrome
-            if palindromeFound.count > longest.count {
-                longest = palindromeFound
+            if  curr - 1 >= 0, curr + 1 < str.count, str[curr - 1] == str[curr + 1] {
+                let solution = expand(str: &str, left: curr - 1, right: curr + 1)
+                if solution.count > longest.count {
+                    longest = solution
+                }
             }
+            if curr + 1 < str.count, str[curr] == str[curr + 1] { //  found 2 char even palindrome such as "aa"
+                let solution = expand(str: &str, left: curr, right: curr + 1)
+                if solution.count > longest.count {
+                    longest = solution
+                }
+            }
+            curr += 1
         }
         return longest
     }
-
-    private func expandAround(curr: inout Int, str: inout [Character]) -> String {
-        typealias Solution = (palindrome: String, right: Int)
-        // set pivotes based on odd vs even palindrome
-        var bestSolution = String(str[curr])
-        if  curr - 1 >= 0, curr + 1 < str.count, str[curr - 1] == str[curr + 1] {
-            let solution = expand(str: &str, left: curr - 1, right: curr + 1)
-            if solution.count > bestSolution.count {
-                bestSolution = solution
-            }
-        }
-        if curr + 1 < str.count, str[curr] == str[curr + 1] { //  found 2 char even palindrome such as "aa"
-            let solution = expand(str: &str, left: curr, right: curr + 1)
-            if solution.count > bestSolution.count {
-                bestSolution = solution
-            }
-        }
-        curr += 1
-        return bestSolution
-    }
-
     private func expand(str: inout [Character], left: Int, right: Int) -> String {
-        var left = left
-        var right = right
-        while left - 1 >= 0, right + 1 < str.count, str[left - 1] == str[right + 1] {
-            left -= 1
-            right += 1
+        var steps = 0
+        while left - (steps + 1) >= 0, right + (steps + 1) < str.count, str[left - (steps + 1)] == str[right + (steps + 1)] {
+            steps += 1
         }
-        return String(str[left...right])
+        return String(str[(left - steps)...(right + steps)])
     }
-
-
 }
 
 let solution = Solution()
