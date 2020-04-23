6d1dff5c9ac655ac1c50df3feddaf6c1f215f90c
Terry Wang <logins.yiwang@gmail.com>
2020-04-23 15:56:34 -0400

    recursive solution

--- LongestPalindromeSubstring.playground/Contents.swift
+++ LongestPalindromeSubstring.playground/Contents.swift
@@ -18,11 +18,10 @@ class Solution {
     func longestPalindrome(_ s: String) -> String {
         if s.count <= 1 { return s }
         var str = Array(s)
-        var longest = String(str[0])
+        var longest = String()
         var curr = 0
         while curr < str.count {
             // iterate on i until we reach a valid palindrome (aa or aba), then expand and store.
-            //print("curr: \(str[curr]) at index: \(curr)")
             let palindromeFound = expandAround(curr: &curr, str: &str) // at min this will be a pathetic 1 char palindrome
             if palindromeFound.count > longest.count {
                 longest = palindromeFound
@@ -33,18 +32,16 @@ class Solution {
 
     private func expandAround(curr: inout Int, str: inout [Character]) -> String {
         typealias Solution = (palindrome: String, right: Int)
-        // Identify if this is a palindrome, and if its and odd (aba) or even (aa) palindrome
-
-        // set pivotes
+        // set pivotes based on odd vs even palindrome
         var bestSolution = String(str[curr])
-        if  curr - 1 >= 0 && curr + 1 < str.count && str[curr - 1] == str[curr + 1] { // found 3 char odd going left and right, such as "bab"
-            let solution = expandPalindrome(str: &str, left: curr - 1, right: curr + 1)
+        if  curr - 1 >= 0, curr + 1 < str.count, str[curr - 1] == str[curr + 1] {
+            let solution = expand(str: &str, left: curr - 1, right: curr + 1)
             if solution.count > bestSolution.count {
                 bestSolution = solution
             }
         }
         if curr + 1 < str.count, str[curr] == str[curr + 1] { //  found 2 char even palindrome such as "aa"
-            let solution = expandPalindrome(str: &str, left: curr, right: curr + 1)
+            let solution = expand(str: &str, left: curr, right: curr + 1)
             if solution.count > bestSolution.count {
                 bestSolution = solution
             }
@@ -53,7 +50,7 @@ class Solution {
         return bestSolution
     }
 
-    private func expandPalindrome(str: inout [Character], left: Int, right: Int) -> String {
+    private func expand(str: inout [Character], left: Int, right: Int) -> String {
         var left = left
         var right = right
         while left - 1 >= 0, right + 1 < str.count, str[left - 1] == str[right + 1] {
@@ -62,6 +59,8 @@ class Solution {
         }
         return String(str[left...right])
     }
+
+
 }
 
 let solution = Solution()
@@ -70,3 +69,17 @@ solution.longestPalindrome("aaaa")
 solution.longestPalindrome("bazzadazz")
 solution.longestPalindrome("bdYdTada")
 print("done")
+
+
+
+
+
+extension Solution {
+    private func expandRecursively(str: inout [Character], left: Int, right: Int) -> String {
+        // base condition
+        if left - 1 < 0 || right + 1 >= str.count || str[left - 1] != str[right + 1] {
+            return String(str[left...right])
+        }
+        return expandRecursively(str: &str, left: left - 1, right: right + 1)
+    }
+}
