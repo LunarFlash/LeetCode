e7b73931a7c19e77771c7b9f44c022ddb1e96aad
Terry Wang <logins.yiwang@gmail.com>
2020-04-28 16:41:18 -0400

    binary search

---
+++ BinarySearch.playground/Contents.swift
@@ -0,0 +1,25 @@
+/**
+ [Binary Search](https://learnappmaking.com/binary-search-swift-how-to/)
+ */
+import UIKit
+
+let numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+func binarySearch(in numbers: [Int], for value: Int) -> Int? {
+    var left = 0
+    var right = numbers.count - 1
+
+    while left <= right {
+        let middle = Int(floor(Double(left + right) / 2.0))
+        if numbers[middle] < value {
+            left = middle + 1
+        } else if numbers[middle] > value {
+            right = middle - 1
+        } else {
+            return middle
+        }
+    }
+    return nil
+}
+
+binarySearch(in: numbers, for: 7)
